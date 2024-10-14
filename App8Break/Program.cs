fb0cf0c98fe3394b2702bc0c65a24b70ed0ead8e
Vivens Ndatinya <ndatinya08@gmail.com>
2024-10-14 06:56:27 -0500

    break keyword

---
+++ App8Break/Program.cs
@@ -0,0 +1,16 @@
+﻿for (int i = 9; i >= 0; i--)
+{
+    if(i == 5) { 
+        break;
+    }
+    
+    for (int k = i; k >=0; k--) 
+    {  
+        if(k == 3) { 
+            break; 
+        }
+
+        Console.Write(k + " ");
+    }
+    Console.WriteLine();
+}
---
+++ NOTE8_BREAK.md
@@ -0,0 +1,4 @@
+# Break keyword
+* Use the `break` keyword to exit a loop
+    - `break` might be useful when you want to stop when a specific value in the collection is reached.
+    
