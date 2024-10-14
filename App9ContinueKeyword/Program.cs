e81f4c2f83016cb5a918fde8e2f7a5a349e06c93
Vivens Ndatinya <ndatinya08@gmail.com>
2024-10-14 07:13:16 -0500

    continue keyword

---
+++ App9ContinueKeyword/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 9; i >= 0; i--)
+{
+    if(i == 8) {
+        continue;
+    }
+
+    for (int k = i; k >= 0; k--)
+    {
+        if(k == 3) { 
+            continue;
+        }
+
+        Console.Write(k + " ");
+    }
+
+    Console.WriteLine();
+}
---
+++ NOTE9_CONTINUE_KEYWORD.md
@@ -0,0 +1,3 @@
+# Continue Keyword
+* The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+
